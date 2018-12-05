b66190c486cdbe8a3ff74b0041231efead0a58dd
Sam Havens <samhavens@gmail.com>
2018-12-05 11:49:20 -0800

    update readme and version

--- README.md
+++ README.md
@@ -12,3 +12,24 @@ This is just a hack for one-off examples. It uses the API in a way that may not 
 
 The plan is to replace the implementation under the hood to use the official API, but this is the POC.
 
+### Usage
+
+```sh
+Usage:
+$ roundtrip text_to_translate to_lang from_lang
+from_lang is optional
+to_lang is required
+Example:
+$ roundtrip "Returns the translation using google translate you must shortcut the language you define" fr
+Returns the translation using Google Translate, you must shorten the language you set
+```
+
+or from python:
+
+```py
+from roundtrip import backtranslate
+
+back = backtranslate("this is a phrase in my NLP training data", "de")
+print(back)
+>>> This is a sentence in my NLP training data
+```
--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ from distutils.core import setup
 setup(
     name = 'roundtrip',
     packages = ['roundtrip'],
-    version = '0.1',
+    version = '0.0.1a1',
     description = 'round trip translation for NLP data augmentation',
     author = 'Sam Havens',
     author_email = 'samhavens@gmail.com',
     url = 'https://github.com/samhavens/roundtrip',
-    download_url = 'https://github.com/samhavens/roundtrip/tarball/0.1',
+    download_url = 'https://github.com/samhavens/roundtrip/tarball/0.0.1a1',
     keywords = ['console', 'translate', 'translator', 'backtranslate', 'roundtrip'],
     classifiers = [],
+    install_requires=['mtranslate'],
     entry_points={
         'console_scripts': [
             'roundtrip = roundtrip.__main__:main'
