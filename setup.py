2a56f0cdb3c9a53f9c879ec3578929cb53e18bcd
Sam Havens <samhavens@gmail.com>
2018-12-05 16:43:01 -0800

    bump version to 0.0.1a2

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from distutils.core import setup
 setup(
     name = 'roundtrip',
     packages = ['roundtrip'],
-    version = '0.0.1a1',
+    version = '0.0.1a2',
     description = 'round trip translation for NLP data augmentation',
     author = 'Sam Havens',
     author_email = 'samhavens@gmail.com',
     url = 'https://github.com/samhavens/roundtrip',
-    download_url = 'https://github.com/samhavens/roundtrip/tarball/0.0.1a1',
+    download_url = 'https://github.com/samhavens/roundtrip/tarball/0.0.1a2',
     keywords = ['console', 'translate', 'translator', 'backtranslate', 'roundtrip'],
     classifiers = [],
-    install_requires=['mtranslate'],
+    install_requires=['mtranslate', 'google-cloud'],
     entry_points={
         'console_scripts': [
             'roundtrip = roundtrip.__main__:main'
